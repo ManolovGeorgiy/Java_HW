fae99e2d0715465d5df5685a9c6a08a784678ad3
ManolovGeorgiy <137941916+ManolovGeorgiy@users.noreply.github.com>
2023-07-22 11:04:12 +0200

    Add files via upload

---
+++ HM_17.07.23/src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int intVal = 23;
+        byte byteVal = (byte)intVal;
+        byte byteVal1 = 10;
+        int intVal1 = byteVal1;
+        System.out.println(byteVal);
+        System.out.println();
+        System.out.println(intVal1);
+        System.out.println();
+
+        byte byteVal2 = 5;
+        int intVal2 = byteVal2;
+        int intVal5 = 13;
+        byte byteVal3 = (byte)intVal5;
+        System.out.println(intVal2);
+        System.out.println();
+        System.out.println(byteVal3);
+        System.out.println();
+
+
+        float floatVal = 455.2f;
+        int intVal3 = (int)floatVal;
+        int intVal6 = 68;
+        float floatVal4 = intVal6;
+        System.out.println(intVal3);
+        System.out.println();
+        System.out.println(floatVal4);
+        System.out.println();
+
+
+        double doubleVal = 4765;
+        float floatVal2 = (float)doubleVal;
+        float floatVal1 = 33;
+        double doubleVal2 = floatVal1;
+        System.out.println(floatVal2);
+        System.out.println();
+        System.out.println(doubleVal2);
+
+
+
+
+    }
+}
+
+
+
+
+
